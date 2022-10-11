a2d61f799858b92068666b8584b3b53d56962354
wanghsinche <wanghsinche@hotmail.com>
2022-10-11 09:42:54 +0000

    padding function

--- utils/date.ts
+++ utils/date.ts
@@ -1,6 +1,11 @@
+export function paddingZero(t:string, bit:number){
+    const zeros = Array(bit).fill('0').join('');
+    const rest = Math.max(bit - t.length, 0);
+    return zeros.slice(0, rest)+t
+}
 export function formatDate(d:Date){
     const realMonth = d.getMonth()+1
-    const month =  realMonth >= 10? String(realMonth): '0'+ realMonth;
-
-    return `${d.getFullYear()}-${month}-${d.getDate()}`
+    const month =  paddingZero(String(realMonth), 2)
+    const date = paddingZero(String(d.getDate()), 2);
+    return `${d.getFullYear()}-${month}-${date}`
 }
