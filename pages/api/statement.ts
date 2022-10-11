fe0a1e6aa2d104c9f223bdae5104ff7c953ae132
wanghsinche <wanghsinche@hotmail.com>
2022-10-11 10:07:17 +0000

    the statement sheet

--- pages/api/statement.ts
+++ pages/api/statement.ts
@@ -4,9 +4,15 @@ import axios from 'axios';
 import { EStatement, IFinvizStatement } from '../../types/finviz';
 const finvizURL = 'https://finviz.com/api/statement.ashx';
 
+type TCell = string|number;
+
+
 export default async function statement(
   req: NextApiRequest,
-  res: NextApiResponse<IFinvizStatement>
+  res: NextApiResponse<{
+    ticker: string;
+    sheetData: TCell[][];
+  }>
 ) {
   const ticker = req.query.ticker as string;
   const sheet = req.query.sheet as EStatement;
@@ -18,6 +24,16 @@ export default async function statement(
   query.set('t', ticker);
 
   const finvizRes = await axios.get(`${finvizURL}?${query.toString()}`)
+  const finvizData = finvizRes.data as IFinvizStatement;
 
-  res.status(200).json(finvizRes.data);
+  const sheetData:TCell[][] = [['',...finvizData.data['Period End Date']]];
+
+  Object.entries(finvizData.data).forEach(([k, v])=>{
+    if (k === 'Period End Date') return
+    sheetData.push([k, ...v])
+  })
+
+  res.status(200).json({
+    ticker, sheetData
+  })
 }
