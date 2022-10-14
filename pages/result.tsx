d129f1f8c9965239c07abaab67e828921f02cbfd
wanghsinche <wanghsinche@hotmail.com>
2022-10-14 05:40:03 +0000

    almost done

--- pages/result.tsx
+++ pages/result.tsx
@@ -9,25 +9,37 @@ const Page: NextPage = () => {
   const router = useRouter()
   const sessionId = router.query.session_id;
 
-  const title = sessionId ? 'Thank you for your payment!':'Payment failed'
+  const title = sessionId ? 'Thanks for your payment!' : 'Payment failed'
 
-  const result = sessionId? <div className='text-ms'>Please reload your spreadsheet to retrieve the latest status.<p>You will receive an email confirmation.</p></div>:'';
+  const result = sessionId ? <div className='text-ms'>Please reload the spreadsheet to retrieve the latest status.<p>You will receive an email confirmation.</p></div> : '';
+
+  const customerPortal = <a href='https://billing.stripe.com/p/login/28o3eV8FO1BJ7Mk000' target='_blank' className='mx-auto lg:mx-0 bg-white text-gray-800 font-bold rounded my-6 py-4 px-8 shadow-lg'>
+    Customer Portal
+  </a>
+
+  const extraBtn = sessionId ? <><p className='text-ms mt-20'>
+    Use Customer Portal to review your subscription plan
+  </p><div className='mt-10'>
+      {customerPortal}
+    </div></> : ''
 
   return (
     <div >
-      <section className="w-full mx-auto text-center bg-gray-100 pt-6 pb-12" style={{height:'50vh'}}>
-      <h2 className="w-full my-2 text-5xl font-black leading-tight text-center">
-        {title}
-      </h2>
-      <div className="w-full mb-4">
-        <div className="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
-      </div>
+      <section className="w-full mx-auto text-center bg-gray-100 pt-6 pb-12" style={{ height: '50vh' }}>
+        <h2 className="w-full my-2 text-5xl font-black leading-tight text-center">
+          {title}
+        </h2>
+        <div className="w-full mb-4">
+          <div className="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
+        </div>
 
-      <h3 className="my-4 text-3xl ">
-        {result}
-      </h3>
+        <h3 className="my-4 text-3xl ">
+          {result}
+        </h3>
 
-    </section>
+        {extraBtn}
+
+      </section>
 
     </div>
   )
