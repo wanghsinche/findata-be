1fd7c11a31525dcd8683de06af4e0a8d7d5dc40e
wanghsinche <wanghsinche@hotmail.com>
2022-10-16 14:38:39 +0000

    yahoo finance

---
+++ pages/api/yahoo-finance.ts
@@ -0,0 +1,86 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getUserPlan } from '../../utils/stripe';
+import { get, primitiveData, tabularData, TCell } from '../../utils/common';
+import Joi from 'joi'
+import yahooFinance from 'yahoo-finance2';
+
+
+interface IResult {
+    error?: string;
+    ticker: string;
+    sheetData: TCell[][];
+}
+
+enum EModule {
+    historicalc='historical', // - historical market prices.
+    quote='quote',// - essential symbol info.
+    quoteSummary='quoteSummary', // - comprehensive symbol info.
+    search='search',// - symbol lookup, news and articles.
+    recommendationsBySymbol='recommendationsBySymbol',// - similar symbols.
+    trendingSymbols='trendingSymbols',// - symbols trending in a country.
+    options='options',// - options trading (call/put).
+    insights='insights',// - insights and scores.
+}
+
+const YHModuleMap:Partial<Record<EModule, (query:string, option: unknown)=>unknown>> = {
+    [EModule.historicalc]: (query:string, option: unknown)=>yahooFinance.historical(query, option as any),
+    [EModule.quote]:(query:string, option: unknown)=>yahooFinance.quote(query, option as any),
+    [EModule.quoteSummary]:(query:string, option: unknown)=>yahooFinance.quoteSummary(query, option as any),
+    [EModule.search]:(query:string, option: unknown)=>yahooFinance.search(query, option as any),
+    [EModule.recommendationsBySymbol]:(query:string, option: unknown)=>yahooFinance.recommendationsBySymbol(query, option as any),
+    [EModule.trendingSymbols]: (query:string, option: unknown)=>yahooFinance.trendingSymbols(query, option as any),
+    [EModule.options]:(query:string, option: unknown)=>yahooFinance.options(query, option as any),
+    [EModule.insights]:(query:string, option: unknown)=>yahooFinance.insights(query, option as any),
+}
+
+const schema = Joi.object({
+    moduleName: Joi.string().required().allow(...Object.values(EModule)),
+    query: Joi.string().required(),
+    queryOptions: Joi.object(),
+    path: Joi.string()
+})
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IResult>
+){
+    if (req.method !== 'POST') return res.status(400).json({ticker:'', sheetData: [], error:'Only For POST'})
+
+    const body = req.body
+    const { error } = schema.validate(body)
+
+    if (error) return res.status(400).json({ticker:'', sheetData: [], error: error.message})
+
+    const moduleName = req.body.moduleName as EModule
+    const query = req.body.query as string
+    const queryOptions = req.body.queryOptions as Record<string, unknown>
+    const path = req.body.path as string
+
+    let data
+
+    try {
+        data = await (YHModuleMap[moduleName] as any)(query, queryOptions as any)
+
+    } catch (err) {
+        return res.status(400).json({ticker:'', sheetData: [], error: String(err)})
+    }
+    
+    const result = !path? data: get(data as any, path, null) as unknown
+
+    if (!result) return res.status(200).json({
+        ticker: query, sheetData: []
+    })
+
+    if (result instanceof Array) return res.status(200).json({
+        ticker: query, sheetData: tabularData(result)
+    })
+
+    if (typeof result === 'object') return res.status(200).json({
+        ticker: query, sheetData: tabularData([result as Record<string, unknown>])
+    })
+
+    return res.status(200).json({
+        ticker: query, sheetData: [[primitiveData(result)]]
+    })
+}
+
--- utils/common.ts
+++ utils/common.ts
@@ -1,4 +1,4 @@
-type TCell = string|number
+export type TCell = string|number
 
 const placeHolder = 'Only For Paid User'
 
@@ -16,3 +16,72 @@ export function coverSomething(arr:TCell[][], rowPos = 2, colPos = 2){
     return arr
 }
 
+
+type TEntity = Record<string, unknown>
+
+export function primitiveData(value:unknown){
+    if (value === null || value === undefined) return ''
+    if (typeof value !== 'object') return value as TCell
+    return String(value)
+}
+
+export function tabularData(data: TEntity[]):TCell[][]{
+    if (data.length === 0) return []
+    const xTicker = Object.keys(data[0])
+
+    const res = data.map(el=>xTicker.map(k=>primitiveData(el[k])))
+
+    // insert the header
+    return [xTicker, ...res]
+}
+
+export function get(obj:Record<string, unknown>, pathRaw: string, def: unknown) {
+
+	/**
+	 * If the path is a string, convert it to an array
+	 * @param  {String} pathRaw The path
+	 * @return {Array}             The path array
+	 */
+	function stringToPath(path: string) {
+		// Create new array
+		const output:string[] = [];
+
+		// Split to an array with dot notation
+		path.split('.').forEach(function (item, index) {
+
+			// Split to an array with bracket notation
+			item.split(/\[([^}]+)\]/g).forEach(function (key) {
+
+				// Push to the new array
+				if (key.length > 0) {
+					output.push(key);
+				}
+
+			});
+
+		});
+
+		return output;
+
+	};
+
+	// Get the path as an array
+	const path = stringToPath(pathRaw);
+
+	// Cache the current object
+	let current: Record<string, unknown> = obj;
+
+	// For each item in the path, dig into the object
+	for (var i = 0; i < path.length; i++) {
+
+		// If the item isn't found, return the default (or null)
+		if (!current[path[i]]) return def;
+
+		// Otherwise, update the current  value
+		current = current[path[i]] as Record<string, unknown>;
+
+	}
+
+	return current;
+
+};
